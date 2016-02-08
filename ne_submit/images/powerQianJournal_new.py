d0629628fd81630825bddb81ca835ac07d7c289f
Evangelos Stromatias <evangelos.stromatias@gmail.com>
2016-02-08 22:33:49 +0100

    fixed some mistakes in the figure

--- ne_submit/images/powerQianJournal_new.py
+++ ne_submit/images/powerQianJournal_new.py
@@ -19,7 +19,7 @@ expresults=[0.27853560652343878,
  0.30354484649472302,
  0.3080469781324936]
 
-nanojoules = [0.0,733.7376571,390.1952438,275.8291809,215.0204542]
+nanojoules = [0.0,735.2634511,392.4909385,274.5132012,213.6658101]
 
 b1=ax.bar([x-0.1 for x in range(0,len(expresults))],expresults,width=0.2,
     color='k',label="Experimental",align="center")
@@ -33,8 +33,10 @@ b2=ax2.bar(
     [x+0.1 for x in range(0,len(expresults))],
     nanojoules,width=0.2,color='b',label="Experimental",align="center")
 
-ax2.set_ylabel("nanoJoules/SE", size=20)
+ax2.set_ylabel("Energy (nJ/SE)", size=20, color='b')
 ax2.tick_params(axis='y', labelsize=15)
+for tl in ax2.get_yticklabels():
+    tl.set_color('b')
 
 ax.set_ylim((0.15,0.32))
 ax.set_xlim((-0.5,len((lbl))-0.5))
