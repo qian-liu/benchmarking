e4b2455f418b8203b421016697f3db57d491721d
Evangelos Stromatias <evangelos.stromatias@gmail.com>
2016-02-08 21:55:02 +0100

    updated figure 12 based on Qian's recomendations. Will add text nnow

---
+++ ne_submit/images/powerQianJournal_new.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 26 17:28:27 2015
+
+@author: Vagoir
+"""
+
+import numpy as np
+import pylab as plt
+
+
+f3=plt.figure()
+ax=f3.add_subplot(111)
+lbl=["0","500","1000","1500","2000"]
+
+expresults=[0.27853560652343878,
+ 0.28559265227641639,
+ 0.29673561810297999,
+ 0.30354484649472302,
+ 0.3080469781324936]
+
+nanojoules = [0.0,733.7376571,390.1952438,275.8291809,215.0204542]
+
+b1=ax.bar([x-0.1 for x in range(0,len(expresults))],expresults,width=0.2,
+    color='k',label="Experimental",align="center")
+
+ax.set_ylabel("Power (w)", size=20)
+ax.set_xlabel("Input firing rate (Hz)", size=20)
+
+ax2 = ax.twinx()
+
+b2=ax2.bar(
+    [x+0.1 for x in range(0,len(expresults))],
+    nanojoules,width=0.2,color='b',label="Experimental",align="center")
+
+ax2.set_ylabel("nanoJoules/SE", size=20)
+ax2.tick_params(axis='y', labelsize=15)
+
+ax.set_ylim((0.15,0.32))
+ax.set_xlim((-0.5,len((lbl))-0.5))
+ax.set_xticks(range(len(lbl)))
+ax.set_xticklabels(lbl)
+ax.tick_params(axis='y', labelsize=15)
+ax.tick_params(axis='x', labelsize=15)
+
+
+
+plt.show()
